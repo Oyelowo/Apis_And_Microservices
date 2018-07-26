235036c85a754385fdc141244a1639b10bff314d
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-07-26 22:05:47 +0300

    mongoose connected to mongo uri

--- Managing_Packages _with_Npm/boilerplate-mongodbmongoose/myApp.js
+++ Managing_Packages _with_Npm/boilerplate-mongodbmongoose/myApp.js
@@ -12,6 +12,7 @@
 // `mongoose`. Store your **mLab** database URI in the private `.env` file 
 // as `MONGO_URI`. Connect to the database using `mongoose.connect(<Your URI>)`
 let moangoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI);
 
 
 /** # SCHEMAS and MODELS #
